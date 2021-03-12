55757df1ab27e2b925a4c5279906ea7962e5b4de
araude0 <ishun928@gmail.com>
2021-03-12 22:34:12 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,163 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import axios from 'axios';
-import Movie from './Movie';
-import "./App.css";
-
-/*
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.9
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 4.8
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 5.5
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.7
-  }
-];
-
-//props.fav = { fav }
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I love {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
+import {HashRouter, Route} from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Detail from './routes/Detail';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div>
-      <h1>Hello!!!!!</h1>
-      {foodILike.map(dish => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-    
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
   );
 }
-*/
-
-// <Mounting> constructor -> render -> componentDidMount
-// <Updating> render -> componentDidUpdate
-// <Unmount> componentWillUnmount
-/*
-class App extends React.Component {
-  state = {
-    count: 0
-  }
-
-  // setState를 호출하게되면 react는 새로운 state를 가지고 리렌더링한다
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  }
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1}));
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
-  }
-}
-*/
-
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  };
-  // async , await -> axios가 끝날 때까지 기다렸다가 계속
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies }
-      }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    // { movies(state의): movies(axios에서 온)} = { movies } 알아서 인식함
-    this.setState({ movies, isLoading: false });
-  };
-  componentDidMount() {
-    // setTimeout(() => {
-    //   this.setState({ isLoading: false });
-    // }, 6000);
-    
-    this.getMovies();
-  }
-  render() {
-    // this.state 오브젝트 안에서 isLoading을 가져온다
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container" >
-        {isLoading ? (
-          <div className ="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map(movie => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
-}
-
-
 
 export default App;
